2048d38b3114b76416e43ca7953fe585a3c3abb8
Amit Chahar <amit.chahar@wispero.com>
2018-06-08 22:41:23 +0530

    code refined: omitted extra pty logic

--- bcs_pty.py
+++ bcs_pty.py
@@ -1,147 +1,36 @@
-"""Pseudo terminal utilities."""
+# Copyright (c) 2018 Amit Chahar
+#
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
 
-# Bugs: No signal handling.  Doesn't set slave termios and window size.
-#       Only tested on Linux.
-# See:  W. Richard Stevens. 1992.  Advanced Programming in the
-#       UNIX Environment.  Chapter 19.
-# Author: Steen Lumholt -- with additions by Guido.
 
 from select import select
 import os
 import tty
 
-__all__ = ["openpty","fork","spawn"]
-
 STDIN_FILENO = 0
 STDOUT_FILENO = 1
 STDERR_FILENO = 2
 
-CHILD = 0
 
-def openpty():
-    """openpty() -> (master_fd, slave_fd)
-    Open a pty master/slave pair, using os.openpty() if possible."""
-
-    try:
-        return os.openpty()
-    except (AttributeError, OSError):
-        pass
-    master_fd, slave_name = _open_terminal()
-    slave_fd = slave_open(slave_name)
-    return master_fd, slave_fd
-
-def master_open():
-    """master_open() -> (master_fd, slave_name)
-    Open a pty master and return the fd, and the filename of the slave end.
-    Deprecated, use openpty() instead."""
-
-    try:
-        master_fd, slave_fd = os.openpty()
-    except (AttributeError, OSError):
-        pass
-    else:
-        slave_name = os.ttyname(slave_fd)
-        os.close(slave_fd)
-        return master_fd, slave_name
-
-    return _open_terminal()
-
-def _open_terminal():
-    """Open pty master and return (master_fd, tty_name).
-    SGI and generic BSD version, for when openpty() fails."""
-    try:
-        import sgi
-    except ImportError:
-        pass
-    else:
-        try:
-            tty_name, master_fd = sgi._getpty(os.O_RDWR, 0666, 0)
-        except IOError, msg:
-            raise os.error, msg
-        return master_fd, tty_name
-    for x in 'pqrstuvwxyzPQRST':
-        for y in '0123456789abcdef':
-            pty_name = '/dev/pty' + x + y
-            try:
-                fd = os.open(pty_name, os.O_RDWR)
-            except os.error:
-                continue
-            return (fd, '/dev/tty' + x + y)
-    raise os.error, 'out of pty devices'
-
-def slave_open(tty_name):
-    """slave_open(tty_name) -> slave_fd
-    Open the pty slave and acquire the controlling terminal, returning
-    opened filedescriptor.
-    Deprecated, use openpty() instead."""
-
-    result = os.open(tty_name, os.O_RDWR)
-    try:
-        from fcntl import ioctl, I_PUSH
-    except ImportError:
-        return result
-    try:
-        ioctl(result, I_PUSH, "ptem")
-        ioctl(result, I_PUSH, "ldterm")
-    except IOError:
-        pass
-    return result
-
-def fork():
-    """fork() -> (pid, master_fd)
-    Fork and make the child a session leader with a controlling terminal."""
-
-    try:
-        pid, fd = os.forkpty()
-    except (AttributeError, OSError):
-        pass
-    else:
-        if pid == CHILD:
-            try:
-                os.setsid()
-            except OSError:
-                # os.forkpty() already set us session leader
-                pass
-        return pid, fd
-
-    master_fd, slave_fd = openpty()
-    pid = os.fork()
-    if pid == CHILD:
-        # Establish a new session.
-        os.setsid()
-        os.close(master_fd)
-
-        # Slave becomes stdin/stdout/stderr of child.
-        os.dup2(slave_fd, STDIN_FILENO)
-        os.dup2(slave_fd, STDOUT_FILENO)
-        os.dup2(slave_fd, STDERR_FILENO)
-        if (slave_fd > STDERR_FILENO):
-            os.close (slave_fd)
-
-        # Explicitly open the tty to make it become a controlling tty.
-        tmp_fd = os.open(os.ttyname(STDOUT_FILENO), os.O_RDWR)
-        os.close(tmp_fd)
-    else:
-        os.close(slave_fd)
-
-    # Parent and child process.
-    return pid, master_fd
-
-def _writen(fd, data):
+def send_to_server(fd, data):
     """Write all the data to a descriptor."""
     while data != '':
         n = fd.send(data)
         data = data[n:]
 
-def _read(fd):
+
+def read_from_stdin(fd):
     """Default read function."""
     return os.read(fd, 1024)
 
-def socket_recv(fd):
+
+def recv_from_server(fd):
     """Default read function."""
     return fd.recv(1024)
 
-def _copy(master_fd, master_read=socket_recv, stdin_read=_read):
+
+def copy_and_send(master_fd):
     """Parent copy loop.
     Copies
             pty master -> standard output   (master_read)
@@ -150,25 +39,20 @@ def _copy(master_fd, master_read=socket_recv, stdin_read=_read):
     while True:
         rfds, wfds, xfds = select(fds, [], [])
         if master_fd in rfds:
-            data = socket_recv(master_fd)
+            data = recv_from_server(master_fd)
             if not data:  # Reached EOF.
                 fds.remove(master_fd)
             else:
                 os.write(STDOUT_FILENO, data)
         if STDIN_FILENO in rfds:
-            data = stdin_read(STDIN_FILENO)
+            data = read_from_stdin(STDIN_FILENO)
             if not data:
                 fds.remove(STDIN_FILENO)
             else:
-                _writen(master_fd, data)
+                send_to_server(master_fd, data)
 
-def spawn(argv, sock, master_read=_read, stdin_read=_read):
-    """Create a spawned process."""
-    if type(argv) == type(''):
-        argv = (argv,)
-    pid, master_fd = fork()
-    if pid == CHILD:
-        os.execlp(argv[0], *argv)
+
+def start_client_terminal(sock):
     try:
         mode = tty.tcgetattr(STDIN_FILENO)
         tty.setraw(STDIN_FILENO)
@@ -176,9 +60,7 @@ def spawn(argv, sock, master_read=_read, stdin_read=_read):
     except tty.error:    # This is the same as termios.error
         restore = 0
     try:
-        _copy(sock, master_read, stdin_read)
+        copy_and_send(sock)
     except (IOError, OSError):
         if restore:
             tty.tcsetattr(STDIN_FILENO, tty.TCSAFLUSH, mode)
-
-    os.close(master_fd)
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # Copyright (c) 2018 Amit Chahar
-# 
+#
 # This software is released under the MIT License.
 # https://opensource.org/licenses/MIT
 
@@ -13,7 +13,7 @@ addr = None
 # search for the Bluetooth-Server-Shell service
 print("Searching all nearby Bluetooth devices for Bluetooth-Server-Shell...")
 uuid = "347fb489-38fb-4325-898b-ec28b40c3c46"
-service_matches = find_service( uuid = uuid, address = addr )
+service_matches = find_service(uuid=uuid, address=addr)
 
 if len(service_matches) == 0:
     print("couldn't find the Bluetooth-Server-Shell service. Please try again.")
@@ -27,7 +27,7 @@ host = first_match["host"]
 print("connecting to \"%s\" on %s" % (name, host))
 
 # Create the client socket
-sock=BluetoothSocket( RFCOMM )
+sock = BluetoothSocket(RFCOMM)
 sock.connect((host, port))
 
 print("connected.  type stuff")
@@ -35,6 +35,6 @@ while True:
     # data = raw_input()
     # if len(data) == 0: break
     # sock.send(data)
-    bcs_pty.spawn("/bin/bash", sock)
+    bcs_pty.start_client_terminal(sock)
 
 sock.close()
